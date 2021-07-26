09b995ac192b1a6c6c25449c05c5905e7aebe560
D.K <6912915+lwinch2006@users.noreply.github.com>
2021-07-26 16:08:43 +0200

    * Added layout for all pages * Added simple setup for a page based on card * Added setup can be used for articles and forms

--- Dka.Net5.ThreeLayersModern/src/WebUI/Controllers/HomeController.cs
+++ Dka.Net5.ThreeLayersModern/src/WebUI/Controllers/HomeController.cs
@@ -8,5 +8,10 @@ namespace WebUI.Controllers
         {
             return View();
         }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
     }
 }
--- dka.net5.codesamples.web/Program.cs
+++ dka.net5.codesamples.web/Program.cs
@@ -1,23 +1,59 @@
 using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace dka.net5.codesamples.web
 {
     public class Program
     {
+        private static string _environmentName;
+        private static IConfiguration _configuration; 
+        
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var host = BuildWebHost(args);
+            host.Run();        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        private static IHost BuildWebHost(string[] args)
+        {
+            BuildParameters();
+            BuildConfiguration();
+
+            var webHost = Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    var urls = _configuration.GetSection("host:urls")
+                        .AsEnumerable()
+                        .Select(t => t.Value)
+                        .Where(t => t != null)
+                        .ToArray();
+                        
+                    webBuilder.UseEnvironment(_environmentName);
+                    webBuilder.UseConfiguration(_configuration);
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls(urls);
+                })
+                .Build();
+
+            return webHost;            
+        }
+        
+        private static void BuildParameters()
+        {
+            _environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        } 
+        
+        private static void BuildConfiguration()
+        {
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddEnvironmentVariables()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{_environmentName}.json", optional: true, reloadOnChange: true)
+                .Build();
+        }
     }
 }
