4c654d664e406e3288d7d3ee5a3f5015f2330a60
D.K <6912915+lwinch2006@users.noreply.github.com>
2021-12-13 22:18:33 +0100

    * Fixed

--- Dka.Net.AzureFunctions/ThreeLayersModernEventsSubscriber/ThreeLayersModernEventsSubscriber.cs
+++ Dka.Net.AzureFunctions/ThreeLayersModernEventsSubscriber/ThreeLayersModernEventsSubscriber.cs
@@ -2,17 +2,21 @@
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host.Bindings;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace ThreeLayersModernEventsSubscriber;
 
 public class ThreeLayersModernEventsSubscriber
 {
     private readonly ILogger<ThreeLayersModernEventsSubscriber> _logger;
+    private readonly ExecutionContextOptions _executionContextOptions;
     
-    public ThreeLayersModernEventsSubscriber(ILogger<ThreeLayersModernEventsSubscriber> logger)
+    public ThreeLayersModernEventsSubscriber(ILogger<ThreeLayersModernEventsSubscriber> logger, IOptions<ExecutionContextOptions> executionContextOptions)
     {
         _logger = logger;
+        _executionContextOptions = executionContextOptions.Value;
     }
     
     [FunctionName("ThreeLayersModernEventsSubscriber")]
@@ -20,8 +24,8 @@ public class ThreeLayersModernEventsSubscriber
         [ServiceBusTrigger("events", "threelayersmoderneventssubscriber", Connection = "ServiceBus:ConnectionString")] string messageAsJson)
     {
         // TODO: Processing logic goes here...
-
-        await using (File.Create($"test-{DateTime.Now.Ticks}.txt")) {}
+        
+        _logger.LogInformation("Current directory: {Directory}", _executionContextOptions.AppDirectory);
         
         _logger.LogInformation("Received topic/subscription {Event} event - raw data {RawData}", "unknown", messageAsJson.Replace(Environment.NewLine, string.Empty));
     }
