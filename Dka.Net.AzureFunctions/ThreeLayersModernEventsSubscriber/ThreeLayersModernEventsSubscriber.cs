626f1a555dd89f00e900aa42461c74cf3cb519cd
D.K <6912915+lwinch2006@users.noreply.github.com>
2021-12-13 20:25:18 +0100

    * Fixed

--- Dka.Net.AzureFunctions/ThreeLayersModernEventsSubscriber/ThreeLayersModernEventsSubscriber.cs
+++ Dka.Net.AzureFunctions/ThreeLayersModernEventsSubscriber/ThreeLayersModernEventsSubscriber.cs
@@ -3,7 +3,6 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Serilog;
-using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace ThreeLayersModernEventsSubscriber;
 
@@ -18,12 +17,12 @@ public class ThreeLayersModernEventsSubscriber
     
     [FunctionName("ThreeLayersModernEventsSubscriber")]
     public async Task RunAsync(
-        [ServiceBusTrigger("events", "threelayersmoderneventssubscriber", Connection = "ServiceBus:ConnectionString")] string messageAsJson, ILogger log)
+        [ServiceBusTrigger("events", "threelayersmoderneventssubscriber", Connection = "ServiceBus:ConnectionString")] string messageAsJson, Microsoft.Extensions.Logging.ILogger log)
     {
         // TODO: Processing logic goes here...
         
         _logger.LogInformation("(1) Received topic/subscription {Event} event - raw data {RawData}", "unknown", messageAsJson.Replace(Environment.NewLine, string.Empty));
         log.LogInformation("(2) Received topic/subscription {Event} event - raw data {RawData}", "unknown", messageAsJson.Replace(Environment.NewLine, string.Empty));
-        Log.Information("(3) Received topic/subscription {Event} event - raw data {RawData}", "unknown", messageAsJson.Replace(Environment.NewLine, string.Empty));
+        //Log.Information("(3) Received topic/subscription {Event} event - raw data {RawData}", "unknown", messageAsJson.Replace(Environment.NewLine, string.Empty));
     }
 }
