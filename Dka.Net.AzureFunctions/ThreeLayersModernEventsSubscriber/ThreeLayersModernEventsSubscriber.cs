ff96898b9aa3496c99a31c86c5b8444d455f03cd
D.K <6912915+lwinch2006@users.noreply.github.com>
2021-12-14 07:56:36 +0100

    * Fixed

--- Dka.Net.AzureFunctions/ThreeLayersModernEventsSubscriber/ThreeLayersModernEventsSubscriber.cs
+++ Dka.Net.AzureFunctions/ThreeLayersModernEventsSubscriber/ThreeLayersModernEventsSubscriber.cs
@@ -24,8 +24,59 @@ public class ThreeLayersModernEventsSubscriber
         [ServiceBusTrigger("events", "threelayersmoderneventssubscriber", Connection = "ServiceBus:ConnectionString")] string messageAsJson)
     {
         // TODO: Processing logic goes here...
+
+        const string fileName = "test-123.txt";
+        
+        
         
         _logger.LogInformation("Current directory: {Directory}", _executionContextOptions.AppDirectory);
+        _logger.LogInformation("Check first file exists: {IsExists}", File.Exists(fileName).ToString());
+
+        if (!File.Exists(fileName))
+        {
+            await File.WriteAllTextAsync(fileName, "Hello World!");
+        }
+        
+        _logger.LogInformation("Check second file exists: {IsExists}", File.Exists(fileName).ToString());
+
+        var info = new FileInfo(fileName);
+        _logger.LogInformation("Fileinfo full file name: {FullFileName}", info.FullName);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        const string dir = "/home/LogFiles/Application/Functions/Function/ThreeLayersModernEventsSubscriber";
+
+        _logger.LogInformation("Check dir exists: {IsExists}", Directory.Exists(dir).ToString());
+
+        if (Directory.Exists(dir))
+        {
+            var fullFileName = Path.Combine(dir, fileName);
+            
+            _logger.LogInformation("Full path: {FullPath}", fullFileName);
+            
+            _logger.LogInformation("Check third file exists: {IsExists}", File.Exists(fullFileName).ToString());
+
+            if (!File.Exists(fullFileName))
+            {
+                await File.WriteAllTextAsync(fullFileName, "Hello World!");
+            }
+        
+            _logger.LogInformation("Check forth file exists: {IsExists}", File.Exists(fullFileName).ToString());
+        }
+        
+        
+        
+        
+        
+        
+        
+        
         
         _logger.LogInformation("Received topic/subscription {Event} event - raw data {RawData}", "unknown", messageAsJson.Replace(Environment.NewLine, string.Empty));
     }
